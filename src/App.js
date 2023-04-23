266c206aec73267a3e9365bf3b41390885cf55a1
youngentry <gentry_@naver.com>
2023-04-23 14:57:15 +0900

    feat: 코멘트 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  useEffect(() => {}, []);
+  const [comments, setComments] = useState([]);
+
+  /**
+   * @param {Array} channelIdArray
+   * @returns
+   */
+  const getAllComments = async (videoId) => {
+    const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+    let nextPageToken = null;
+
+    let comments = [];
+
+    try {
+      do {
+        const params = {
+          key: apiKey,
+          part: "snippet",
+          videoId,
+          order: "relevance",
+          maxResults: 100,
+          pageToken: nextPageToken,
+        };
+        const response = await axios.get("https://www.googleapis.com/youtube/v3/commentThreads", { params });
+        comments = [...comments, ...response.data.items];
+        nextPageToken = response.data.nextPageToken || null;
+      } while (nextPageToken && comments.length < 50);
+
+      return comments;
+    } catch (error) {
+      console.error(error, "getPlaylistItems error");
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      setComments(await getAllComments("jqvCCJ25LiY"));
+    })();
+  }, []);
   return (
     <div className="App">
       <iframe
@@ -14,6 +53,21 @@ function App() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       ></iframe>
+
+      {comments.length &&
+        comments.map((comment) => {
+          const { textDisplay, likeCount, updatedAt, authorDisplayName } = comment.snippet.topLevelComment.snippet;
+          return (
+            <div>
+              <div>{authorDisplayName}</div>
+              <p>{textDisplay}</p>
+              <div>
+                <span>{likeCount}</span>
+                <span>{updatedAt}</span>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
